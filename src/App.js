eff5ad67012c63638094b76b5c1e086198688e3e
Timothy Hoang <timh1203@gmail.com>
2018-09-02 17:43:26 -0400

    Add avenge list and render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import avengers from './avengers'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      avengers: avengers
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Avengers: by Timothy H.</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  
+        <div>
+          {this.state.avengers.map( avenger => (
+            <div key={avenger.id}>
+              <p>
+                Hero Name: {avenger.heroName} ({avenger.realName})
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
---
+++ src/avengers.js
@@ -0,0 +1,19 @@
+const avengers = [
+  { id: 1, heroName: "Iron Man", realName: "Anthony Edward Stark" },
+  { id: 2, heroName: "Thor", realName: "Thor Odinson " },
+  { id: 3, heroName: "Ant-Man", realName: "Dr. Henry Jonathan Pym" },
+  { id: 4, heroName: "Wasp", realName: "Janet van Dyne" },
+  { id: 5, heroName: "Hulk", realName: "Dr. Robert Bruce Banner" },
+  { id: 6, heroName: "Captain America", realName: "Steven Rogers" },
+  { id: 7, heroName: "Hawkeye", realName: "Clinton Francis Barton" },
+  { id: 8, heroName: "Quicksilver", realName: "Pietro Maximoff"},
+
+  { id: 9, heroName: "Scarlet Witch", realName: "Wanda Maximoff" },
+  { id: 10, heroName: "Swordsman", realName: "Jacques Duquesne" },
+  { id: 11, heroName: "Hercules", realName: "Heracles" },
+  { id: 12, heroName: "Black Panther", realName: "T'Challa" },
+  { id: 13, heroName: "Vision", realName: "Victor Shade" },
+  { id: 14, heroName: "Black Knight", realName: "Dane Whitman" }
+]
+
+export default avengers
